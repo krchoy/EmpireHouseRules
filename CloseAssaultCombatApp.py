1acfb6fdebe3bf6ed13d06ad1f184300c2b950a8
krchoy <krchoy@gmail.com>
2023-12-23 19:16:20 +1100

    List to Dict
    Combo list item to dictionary lookup working

--- CloseAssaultCombatApp.py
+++ CloseAssaultCombatApp.py
@@ -3,17 +3,24 @@ from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import Qt
 import sys
  
-class Window(QWidget):
+class CAC_Window(QWidget):
 
     
     def __init__(self):
         super().__init__()
 
-        MoraleGrades_Initial_Row = {'VOL': 10, 'TM': 14, 'LW': 17, 'CON': 18,
+        self.MoraleGrades_Initial_Row = {'VOL': 10, 'TM': 14, 'LW': 17, 'CON': 18,
                                    'VET': 20, 'CRK': 22, 'ELT': 23, 'GRN': 24, 'GRD': 25, 'OG': 27}
         self.MoraleGrades = ['', ]
-        for name in MoraleGrades_Initial_Row.keys():
+        for name in self.MoraleGrades_Initial_Row.keys():
             self.MoraleGrades.append(name)
+            
+        self.CloseAssaultCombatTable = [1, 1, 1, 1, 2, 2, 2, 2, 2, 3,
+                           3, 4, 4, 5, 5, 6, 6, 7, 8, 9,
+                           10, 11, 12, 14, 16, 18, 20, 22, 24, 27,
+                           30, 35, 40, 45, 50, 55, 62, 40, 80, 90,
+                           100, 110, 120, 140, 160, 180, 200, 220, 240, 270,
+                           300, 350, 400, 450, 500, 550, 620, 700, 800, 900]    
 
         self.UI()
     
@@ -26,36 +33,54 @@ class Window(QWidget):
         attackerUI = QVBoxLayout()
         label_1 = QLabel("Attacker Morale Grade")
         label_1.setAlignment(Qt.AlignmentFlag.AlignTop)
-        moraleGradeAttacker = QComboBox(self)
-        moraleGradeAttacker.addItems(self.MoraleGrades)
+        self.moraleGradeAttacker = QComboBox(self)
+        self.moraleGradeAttacker.addItems(self.MoraleGrades)
         attackerUI.addWidget(label_1)
-        attackerUI.addWidget(moraleGradeAttacker)
+        attackerUI.addWidget(self.moraleGradeAttacker)
 
         
         defenderUI = QVBoxLayout()
         label_2 = QLabel("Defender Morale Grade")
         label_2.setAlignment(Qt.AlignmentFlag.AlignTop)
-        moraleGradeDefender = QComboBox(self)
-        moraleGradeDefender.addItems(self.MoraleGrades)
+        self.moraleGradeDefender = QComboBox(self)
+        self.moraleGradeDefender.addItems(self.MoraleGrades)
         defenderUI.addWidget(label_2)
-        defenderUI.addWidget(moraleGradeDefender)
+        defenderUI.addWidget(self.moraleGradeDefender)
 
         setMoraleUI = QVBoxLayout()
         moraleSelectedBN = QPushButton("Morale Selected", self)
+        moraleSelectedBN.clicked.connect(self.MoraleSelected)
         setMoraleUI.addWidget(moraleSelectedBN)
 
-        upperPaneUI = QGridLayout()        
-        upperPaneUI.addLayout(attackerUI, 0, 0)
-        upperPaneUI.addLayout(setMoraleUI, 0, 1)
-        upperPaneUI.addLayout(defenderUI, 0, 2)
-        self.setLayout(upperPaneUI)
+        self.label_3 = QLabel("inert")
+        self.label_3.setAlignment(Qt.AlignmentFlag.AlignBottom)
+
+        self.upperPaneUI = QGridLayout()        
+        self.upperPaneUI.addLayout(attackerUI, 0, 0)
+        self.upperPaneUI.addLayout(setMoraleUI, 0, 1)
+        self.upperPaneUI.addLayout(defenderUI, 0, 2)
+        self.upperPaneUI.addWidget(self.label_3, 1, 1)
+        self.setLayout(self.upperPaneUI)
         self.setGeometry(500, 500, 200, 200)
         self.setWindowTitle('Close Assault Combat')
         self.show()
+
+    def MoraleSelected(self):
+
+#        self.moraleGradeAttacker.hide()
+#        self.moraleGradeDefender.disable()
+        
+        print(self.moraleGradeAttacker.currentText())
+        print(self.moraleGradeAttacker.currentIndex())        
+        print(self.MoraleGrades_Initial_Row[self.moraleGradeAttacker.currentText()])
+        self.label_3.setText(self.moraleGradeAttacker.currentText() + str(self.moraleGradeAttacker.currentIndex()))
+        self.moraleGradeAttackerRowStart = self.MoraleGrades_Initial_Row[self.moraleGradeAttacker.currentIndex()]
+        print(self.moraleGradeAttackerRowStart)
+        print(self.CloseAssaultCombatTable[self.moraleGradeAttackerRowStart])
         
 
- 
+
 app = QApplication(sys.argv)
-window = Window()
+window = CAC_Window()
 window.show()
 sys.exit(app.exec())
