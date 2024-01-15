ae61fd001d0a9d36c6f708b4b1bee7bbd41e2b2a
krchoy <krchoy@gmail.com>
2024-01-15 11:42:34 +1100

    WIP tk implementation

--- CloseAssaultCombat2.py
+++ CloseAssaultCombat2.py
@@ -1,6 +1,7 @@
 """Close Assault Combat Assistant using Tkinter"""
 
 import tkinter as tk
+from tkinter import ttk
 
 class CAC_Window(tk.Tk):
     """Root window """
@@ -26,18 +27,141 @@ class CAC_Window(tk.Tk):
     def UI(self):
         # set the title
         self.title('Close Assault Combat Assistant')
-
         # set the root window size
         self.geometry('640x480+300+300')
         self.resizable(True, True)
+        #use a common frame to input attacker and defender modifiers
+        AttackModifier=0
+        attacker = CombatantEntry(self, "Attacker", AttackModifier)
+        attacker.grid(row=1, column=0)
 
+        label = tk.Label(
+            self,
+            text="space",
+            )
+        label.grid(row=2, column=1)        
+        DefendModifier=0
+        defender = CombatantEntry(self, "Defender", DefendModifier)
+        defender.grid(row=1, column=2)
+        
     
 
+
+class CombatantEntry(tk.Frame):
+    """Frame reused for attaker and defender attributes entry """
+    def __init__(self, parent, combatant, cellModifier):
+        super().__init__(parent)
+
+        
+        self.label = tk.Label(
+            self,
+            text=combatant,
+            anchor='w',
+            font=('TkHeadingFont 16 bold')
+            )
+
+        self.columnconfigure(1, weight=1)
+        self.label.grid(sticky=tk.E + tk.W)
+        self.label = tk.Label(
+            self,
+            text='Modifiers',
+            anchor='w',
+            font=('TkDefaultFont 10 normal')
+            )
+
+        self.columnconfigure(1, weight=1)
+        self.label.grid(sticky=tk.E + tk.W)
+        ##
+        label1 = tk.Label(
+            self,
+            text="Every 2 figures lost since start of day")
+        result1 = tk.Spinbox(
+            self,
+            from_=0,
+            to=12,
+            increment=1)
+        label1.grid()
+        result1.grid()
+        ##
+        label2 = tk.Label(
+            self,
+            text="Every Fatigue Point accumulated")
+        result2 = tk.Spinbox(
+            self,
+            from_=0,
+            to=12,
+            increment=1)
+        label2.grid()
+        result2.grid()
+        ##
+        label3 = tk.Label(
+            self,
+            text="Unit morale is:")
+        tr3 = tk.StringVar()
+        result3 = ttk.Combobox(
+            self,
+            height=1,
+            textvariable=tr3
+            )
+        result3['values'] = (
+            'Good',
+            'Disordered',
+            'Blown',
+            'Blown & Disordered',
+            'Broken' )
+        
+        label3.grid()
+        result3.grid()
+        result3.current(1)
+        
+        ##
+        label4 = tk.Label(
+            self,
+            text="Attached Leadership at:")
+        tr4 = tk.StringVar()
+        result4 = ttk.Combobox(
+            self,
+            height=2,
+            textvariable=tr4
+            )
+        result4['values'] = ("None",
+            "Charismatic Div or Brig",
+            "Charismatic Reg or below",
+            "Inspiring Div or Brig",
+            "Inspiring Reg or below",
+            "Fine Unit leader attached to sub unit",
+            "Contemptible Div or Brig",
+            "Contemptible Reg or below"
+            )
+        
+        label4.grid()
+        result4.grid()
+        result4.current(1)       
+
+##
+        label5 = tk.Label(
+            self,
+            text="Unit is attacking:")
+        tr5 = tk.StringVar()
+        result5 = ttk.Combobox(
+            self,
+            height=1,
+            textvariable=tr5
+            )
+        result5['values'] = ("with no advantage",
+            "the Flank",
+            "the Rear",
+            "with Partial Enfilade",
+            "with Full Enfilade"
+            )
+        
+        label5.grid()
+        result5.grid()
+        result5.current(1)       
 
 ###############
 # Execute App #
 ###############
-
-root = CAC_Window()
-root.mainloop() 
-
+if __name__ == '__main__':
+    root = CAC_Window()
+    root.mainloop() 
